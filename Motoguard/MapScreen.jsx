4cc254497bb54d232ae19a60e13c57249c137d77
Ben <benjamin.cottone@epitech.eu>
2023-09-29 16:44:22 +0200

    [+] Add a button to return to the position of the marker on the map

--- Motoguard/MapScreen.jsx
+++ Motoguard/MapScreen.jsx
@@ -1,60 +1,74 @@
-import React, {useState, useEffect} from "react";
-import MapView, {Marker} from "react-native-maps";
-import { StyleSheet, Text, View, Dimensions, Button } from "react-native";
-import * as Location from 'expo-location';
+import React, { useState, useEffect, useRef } from "react";
+import MapView, { Marker } from "react-native-maps";
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import * as Location from "expo-location";
 
 export default function MapScreen() {
-  const [Pseudo, setPseudo] = useState("Benjamin")
-  const [mapRegion, setMapRegion] = useState({
-    latitude: 43.669658,
-    longitude: 7.215516,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0442,
-  });
-  const userLocation = async () => {
-    let {status} = await Location.requestBackgroundPermissionsAsync();
-    if (status !== 'granted') {
-      setErrorMsg('Permission to acces denied');
+  const [Pseudo, setPseudo] = useState("Benjamin");
+  const [userLocation, setUserLocation] = useState(null);
+
+  const mapRef = useRef(null);
+
+  const updateUserLocation = async () => {
+    let { status } = await Location.requestBackgroundPermissionsAsync();
+    if (status !== "granted") {
+      setErrorMsg("Permission to access denied");
     }
-    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
-    setMapRegion({
+    let location = await Location.getCurrentPositionAsync({
+      enableHighAccuracy: true,
+    });
+    setUserLocation({
       latitude: location.coords.latitude,
       longitude: location.coords.longitude,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
     });
+
+    if (mapRef.current) {
+      mapRef.current.animateToRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      });
+    }
+
     console.log(location.coords.latitude, location.coords.longitude);
-  }
+  };
 
   useEffect(() => {
-    userLocation();
+    updateUserLocation();
   }, []);
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}
-      region={mapRegion}
-      >
-      <Marker coordinate={mapRegion} title={`${Pseudo}'s marker`} />
-      </MapView>
-      <Button title='Get Location' onPress={userLocation} style={styles.button} />
+      <MapView
+        ref={mapRef}
+        style={styles.map}
+        showsUserLocation={true}
+        onRegionChangeComplete={() => {}}
+      ></MapView>
+
+      {userLocation && (
+        <Marker coordinate={userLocation} title={`${Pseudo}'s marker`} />
+      )}
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    map: {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    },
-    button: {
-      position: 'absolute',
-      top: 0,
-      left: 0,
-      zIndex: 100,
-    }
-  })
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height-40,
+  },
+});
